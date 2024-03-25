a35cf176ab1172771ab2dfcbedd7aa2a42a1df39
RUHAMYA Elie <elieruhamya20@gmail.com>
2024-03-25 17:29:28 +0200

    new update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -207,43 +207,16 @@
               </button>
             </div>
           </form>
-          <div class="d-flex justify-content-end">
-            <button
-              class="btn btn-primary btn-sm bg-transparent border-0 d-flex align-items-center"
-              type="button"
-              style="padding: 0px; font-size: 12px; color: #013854; margin-top: 10px"
-              data-bs-target="#about"
-              data-bs-toggle="modal"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                width="1em"
-                height="1em"
-                fill="currentColor"
-                style="margin-right: 4px"
-              >
-                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
-                <path
-                  d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
-                ></path></svg
-              >About
-            </button>
-          </div>
         </div>
       </div>
     </div>
   </div>
-  <ModalView />
 </template>
 
 <script>
 import axios from 'axios'
-import ModalView from '../components/Modal.vue'
+// import ModalView from '../components/Modal.vue'
 export default {
-  components: {
-    ModalView
-  },
   data() {
     return {
       error: false,
